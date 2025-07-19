b91412222cf50c6cc4579107c90440ab8a86533f
abhi-ghute <abghute7@gmail.com>
2025-07-19 22:22:50 +0530

    Basic authentication implementation

---
+++ SpringSecurityDemoApplication/src/main/java/com/example/springsecuritydemo/controller/GreetingController.java
@@ -0,0 +1,15 @@
+package com.example.springsecuritydemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class GreetingController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello";
+    }
+}
