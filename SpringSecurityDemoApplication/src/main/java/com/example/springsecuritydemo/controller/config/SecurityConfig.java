53ce68af83781e636b0b3d0e69c686de9a33dbe7
abhi-ghute <abghute7@gmail.com>
2025-07-20 19:29:19 +0530

    Integration of h2 db with spring security

--- SpringSecurityDemoApplication/src/main/java/com/example/springsecuritydemo/controller/config/SecurityConfig.java
+++ SpringSecurityDemoApplication/src/main/java/com/example/springsecuritydemo/controller/config/SecurityConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,8 +18,11 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> {
+            requests.requestMatchers("/h2-console/**").permitAll();
             requests.anyRequest().authenticated();
         });
+        http.csrf(csrf->csrf.ignoringRequestMatchers("/h2-console/**"));
+        http.headers(headers->headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
         http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic(Customizer.withDefaults());
         return http.build();
@@ -41,3 +44,5 @@ public class SecurityConfig {
         return new InMemoryUserDetailsManager(user1,admin);
     }
 }
+
+
