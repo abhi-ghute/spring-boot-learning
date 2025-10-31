925411d5e998e61c2531d86221f0183cfc019966
abhi-ghute <abghute7@gmail.com>
2025-10-31 22:31:04 +0530

    kafka demo app

---
+++ kafka/KafkaExample/src/main/java/com/demo/kafka/service/KafkaProducerService.java
@@ -0,0 +1,19 @@
+package com.demo.kafka.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducerService {
+
+    private static final String TOPIC = "demo-topic";
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send(TOPIC, message);
+        System.out.println("Sent message: " + message);
+    }
+}
