4863db4e7caa0456f5bf149bb6c7cf8490aa69a3
abhi-ghute <abghute7@gmail.com>
2025-12-31 22:25:04 +0530

    spring-data-jpa-hospital-mangement-initial-commit

---
+++ spring-boot-data-jpa-hospital-management-system/src/main/java/com/hospital/management/entity/Patient.java
@@ -0,0 +1,24 @@
+package com.hospital.management.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public final class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDate birthDate;
+
+    private String email;
+
+    private String gender;
+}
